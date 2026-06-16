4f6e955958abbc7fd2556b915435d90bf6584b06
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2026-06-16 20:56:30 +0600

    Update gemini_service.py

--- backend/services/gemini_service.py
+++ backend/services/gemini_service.py
@@ -1,1 +1,20 @@
+import google.generativeai as genai
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#print(os.getenv("GEMINI_API_KEY"))
+
+
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+def ask_gemini(prompt):
+    response = model.generate_content(prompt)
+    return response.text
