3447d5ec13e9a1da818079e12f40ef18e1b612c1
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2026-06-16 20:56:11 +0600

    Update memory_service.py

--- backend/services/memory_service.py
+++ backend/services/memory_service.py
@@ -1,1 +1,18 @@
+from database.db import SessionLocal
+from database.models import Memory
 
+
+def save_memory(text, category):
+
+    db = SessionLocal()
+
+    memory = Memory(
+        memory_text=text,
+        category=category
+    )
+
+    db.add(memory)
+
+    db.commit()
+
+    db.close()
