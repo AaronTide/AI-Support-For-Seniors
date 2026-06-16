0e75ffdf50fbc483994db22a3c9b38749da33e9d
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2026-06-16 20:57:28 +0600

    Update memory.py to save and retrieve memories

---
+++ backend/routes/memory.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from services.memory_service import save_memory
+
+router = APIRouter()
+
+
+class MemoryRequest(BaseModel):
+    text: str
+    category: str
+
+
+@router.post("/memory")
+def add_memory(req: MemoryRequest):
+
+    save_memory(
+        req.text,
+        req.category
+    )
+
+    return {
+        "message": "memory saved"
+    }
+
+from database.db import SessionLocal
+from database.models import Memory
+
+
+@router.get("/memories")
+def get_memories():
+
+    db = SessionLocal()
+
+    memories = db.query(Memory).all()
+
+    results = []
+
+    for m in memories:
+
+        results.append({
+            "id": m.id,
+            "text": m.memory_text,
+            "category": m.category
+        })
+
+    db.close()
+
+    return results
