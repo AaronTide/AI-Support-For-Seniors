0b735bb4e946beda1ec588b5c80f72770bf523d7
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2026-06-17 16:09:29 +0600

    pass semantic memory in prompt

--- backend/routes/chat.py
+++ backend/routes/chat.py
@@ -2,18 +2,36 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from services.gemini_service import ask_gemini
-
+from services.memory_service import get_relevant_memories
 router = APIRouter()
 
 class ChatRequest(BaseModel):
     message: str
 
 
+
 @router.post("/chat")
 def chat(req: ChatRequest):
+    memories = get_relevant_memories(
+    req.message
+)
+    memory_context = "\n".join(memories)
+    
+    #answer = ask_gemini(req.message)
+    prompt = f"""
+    You are SilverBridge AI.
 
-    answer = ask_gemini(req.message)
+    Relevant User Memories:
+    {memory_context}
 
+    User Question:
+    {req.message}
+
+    Use the memories if they are relevant.
+    If not relevant, answer normally.
+    """
+
+    answer = ask_gemini(prompt)
     return {
         "response": answer
     }
